41c5aca19aaac3c5a128fb6e62415c85f69b292c
Purpurova-k <katrin.purpurova@gmail.com>
2021-11-01 17:46:38 +0300

    IDEA project added

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+        public int calculate(int price) {
+            int mile = 20;
+            int miles = price / mile;
+            return miles;
+        }
+    }
